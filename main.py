de10140abae861003f93a2c48e9e74275206b281
Chungeun Choi <ce.choi@okestro.com>
2024-01-31 08:36:18 +0900

    feature - proxycurl의 호출 제한으로 인해 데이터를 gits에서 조회할 수 있도록 기능추가

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from third_parties.linkedin import scrape_linkedin_profile
+import json
+
+from third_parties.linkedin import scrape_linkedin_profile, scrape_gits_profile
 
 if __name__ == "__main__":
-    linked_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/chungeun-choi-202585257/"
-    )
-    print(linked_data)
+    # linked_data = scrape_linkedin_profile(
+    #     linkedin_profile_url="https://www.linkedin.com/in/chungeun-choi-202585257/"
+    # )
+    # print(linked_data)
+
+    url = "https://gist.githubusercontent.com/chungeun-choi/efdfdb4945025fc1690c4ec9ca0e5972/raw/d4b8e558b3617c7aa6ea8baee1cd787fc49a2de3/choi-chungeun.json"
+    gits_data = scrape_gits_profile(url)
+    print(gits_data)
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -2,8 +2,25 @@ import os
 import requests
 
 
+def scrape_gits_profile(gits_url: str):
+    response = requests.get(gits_url)
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
 def scrape_linkedin_profile(linkedin_profile_url: str):
-    api_endpoint = "https://nubela.co/proxyurl/api/v2/linkedin"
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
     header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYURL_API_KEY")}'}
 
     response = requests.get(
